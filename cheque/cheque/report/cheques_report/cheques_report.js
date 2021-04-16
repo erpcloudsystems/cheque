86e9eae346fe0a329b976f6e291d8cbe39acff53
mghoneem10 <mghoneem10@gmail.com>
2021-04-16 21:08:55 +0200

    add cheque report

--- cheque/cheque/report/cheques_report/cheques_report.js
+++ cheque/cheque/report/cheques_report/cheques_report.js
@@ -1,9 +1,38 @@
-// Copyright (c) 2016, erpcloud.systems and contributors
+// Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 /* eslint-disable */
 
 frappe.query_reports["Cheques Report"] = {
 	"filters": [
-
+		{
+			"fieldname":"type",
+			"label": __("Type"),
+			"fieldtype": "Select",
+			"options": ["Pay","Receive","Internal Transfer"]
+		},
+		{
+			"fieldname":"status",
+			"label": __("Status"),
+			"fieldtype": "Select",
+			"options":  ["جديد","مظهر","تحت التحصيل","محصل","مرفوض بالبنك","حافظة شيكات مرجعة","مردود","مدفوع","محصل فوري"]
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_start_date")
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_user_default("year_end_date")
+		},
+		{
+			"fieldname":"bank",
+			"label": __("Bank"),
+			"fieldtype": "Link",
+			"options": "Bank Account"
+		}
 	]
-};
+}
