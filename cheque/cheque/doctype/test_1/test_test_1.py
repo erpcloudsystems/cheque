2acaa8af93f181973582c071f4138bcd106f8c84
mghoneem10 <mghoneem10@gmail.com>
2021-04-16 16:13:11 +0200

    add doctype final

---
+++ cheque/cheque/doctype/test_1/test_test_1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, erpcloud.systems and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class Testtest_1(unittest.TestCase):
+	pass
